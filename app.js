e0c4508be451a4e97eef0ab4b0f8dd2f965095ba
Santiago Tourn <111823540+HSPower255@users.noreply.github.com>
2023-02-22 00:40:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+app.use(cors());
+app.options('*', cors())
+
+
+//MIDDLEWARES
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use(errorHandler);
+
+
+//ROUTES
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+
+//DATABASE
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
+
+
+//SERVER
+app.listen(3000, ()=>{
+
+    console.log('Server is Running');
+})
